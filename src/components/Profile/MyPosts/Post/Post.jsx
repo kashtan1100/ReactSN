87e02c82129b6fd4f94df2ad4152926fb186a1f5
Kashtan1100 <otabekxama822@gmail.com>
2021-08-06 13:55:13 +0300

    add reducer refactoring 41

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ import s from './Post.module.css';
 let Post = (props) => {
     return (
         <div className={s.item}>
-            <img src={'https://million-wallpapers.ru/wallpapers/5/51/497634337058431/nejtiri-avatar-film-1.jpg'}
+            <img src={'https://avatars.mds.yandex.net/get-zen_doc/2414075/pub_5fe5b24763337471b9f21113_5fe5b682b590cf1d64c0d9f1/scale_1200'}
             alt={'description'}
             />
             {props.message}
