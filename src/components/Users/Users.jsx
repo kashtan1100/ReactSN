bf876a1713a96bc499b46f20c1cadedbe177bba6
Kashtan1100 <otabekxama822@gmail.com>
2021-08-11 13:20:13 +0300

    pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,16 +6,42 @@ import userPhoto from "../../assets/images/324834163022201.jpg";
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=
+            ${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=
+            ${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
             });
     }
 
     render() {
+        let pagesCount = Math.ceil((this.props.totalUsersCount / this.props.pageSize) / 190);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                                 onClick={(e) => {
+                                     this.onPageChanged(p)
+                                 }}>{p}</span>
+                })}
+            </div>
             {
-            this.props.users.map(u => <div key={u.id}>
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={
@@ -33,7 +59,7 @@ class Users extends React.Component {
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -43,8 +69,8 @@ class Users extends React.Component {
                         <div>{'u.location.city'}</div>
                     </span>
                 </span>
-            </div>)
-        }
+                </div>)
+            }
         </div>
     }
 }
