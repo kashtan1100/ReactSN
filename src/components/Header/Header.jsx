847e010792d056cf90f674088c38e6c182eff9b2
Kashtan1100 <otabekxama822@gmail.com>
2021-08-27 16:42:34 +0300

    cookie, login, auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 
-let Header = () => {
+let Header = (props) => {
     return <header className={s.header}>
         <img
             src={'https://e7.pngegg.com/pngimages/197/457/png-clipart-round-multicolored-logo-vanamo-logo-icons-logos-emojis-tech-companies.png'}
             alt={'description'}
         />
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
