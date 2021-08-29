ca81334e92318997683c290c2a89ee7d5653b8ed
Kashtan1100 <otabekxama822@gmail.com>
2021-08-29 22:20:41 +0300

    DAL, axios.create

---
+++ src/components/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "8661b7a5-8fd0-4ad1-a0a1-09810387298c"
+    }
+})
+
+export const UsersApi = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`, {
+            withCredentials: true,
+        }).then(response => response.data);
+    }
+}
