b7123b34b014b3056bec0bc58a712fb85e248819
Kashtan1100 <otabekxama822@gmail.com>
2021-09-05 23:41:17 +0300

    redux-form введение (login) 75

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -35,7 +35,6 @@ class ProfileStatus extends React.Component {
     }
 
     render() {
-        console.log('render');
         return (
             <div>
                 {!this.state.editMode &&
