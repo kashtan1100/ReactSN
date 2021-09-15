9598e0b435ba40023be128516224216918596a16
Kashtan1100 <otabekxama822@gmail.com>
2021-09-15 16:08:23 +0300

    login и logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,8 @@ let Header = (props) => {
             alt={'description'}
         />
         <div className={s.loginBlock}>
-            {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+            {props.isAuth
+                ? <div>{props.login} - <button onClick={props.logout}>Log out</button></div> : <NavLink to={'/login'}>Login</NavLink>}
         </div>
     </header>
 }
